275e553a0947e4909ce20a820fa956079db14e27
Shakeel Haider <shkhaider2015@gmail.com>
2024-10-29 19:46:22 +0500

    add refrence to edit profile

--- src/screens/EditProfile/EditProfile.tsx
+++ src/screens/EditProfile/EditProfile.tsx
@@ -92,7 +92,7 @@ const EditProfileScreen = ({ navigation }: EditProfileScreenType) => {
       bio: user.bio,
       activitiesToAdd: [],
       activitiesToDelete: [],
-      profession: user.profession
+      profession: user.profession,
     },
     validationSchema: editProfileSchema,
     onSubmit: (values) => {
@@ -140,14 +140,13 @@ const EditProfileScreen = ({ navigation }: EditProfileScreenType) => {
     const fileSize = result.assets?.[0].fileSize || 0;
     const fileSizeInMB = (fileSize / (1024 * 1024)).toFixed(2);
 
-    if(Number(fileSizeInMB) >= 5) {
+    if (Number(fileSizeInMB) >= 5) {
       Toast.show({
-        type: "info",
-        text1: "Image size must be less than 5 MB"
-      })
-      return
+        type: 'info',
+        text1: 'Image size must be less than 5 MB',
+      });
+      return;
     }
-    
 
     formik.setFieldValue('profileImage', result.assets?.[0]);
   };
@@ -170,7 +169,7 @@ const EditProfileScreen = ({ navigation }: EditProfileScreenType) => {
       <EditProfileHeader onBack={_handleBack} onUpdate={_onUpdate} />
       <KeyboardAvoidingView
         behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-        style={[ layout.flex_1 ]}
+        style={[layout.flex_1]}
       >
         <ScrollView>
           <View
@@ -288,6 +287,7 @@ const EditProfileScreen = ({ navigation }: EditProfileScreenType) => {
                 Profession
               </Text>
               <InputField
+                ref={professionRef}
                 placeholder="Profession"
                 onChangeText={formik.handleChange('profession')}
                 onBlur={formik.handleBlur('profession')}
@@ -298,7 +298,9 @@ const EditProfileScreen = ({ navigation }: EditProfileScreenType) => {
                 autoCapitalize="none"
                 blurOnSubmit={false}
                 isError={
-                  formik.touched.profession && formik.errors.profession ? true : false
+                  formik.touched.profession && formik.errors.profession
+                    ? true
+                    : false
                 }
               />
               <Text style={[gutters.marginLeft_12, fonts.size_12, fonts.error]}>
