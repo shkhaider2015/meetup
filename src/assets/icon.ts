71a3477bdfddf3af786994ce004f74736799329f
Shakeel Haider <shkhaider2015@gmail.com>
2024-10-31 16:47:42 +0500

    update status bar component added wifi icon

--- src/assets/icon.ts
+++ src/assets/icon.ts
@@ -58,3 +58,4 @@ export { default as Hide } from "@/assets/icons/Eye2.svg" ;
 export { default as Show } from "@/assets/icons/Eye2Slash.svg";
 export { default as Envelop } from '@/assets/icons/envelop.svg';
 export { default as SettingsIcon } from '@/assets/icons/Setting_line_light.svg';
+export { default as WifiIcon } from "@/assets/icons/wifi.svg";
--- src/components/NetworkStatusBar/NetworkStatusBar.tsx
+++ src/components/NetworkStatusBar/NetworkStatusBar.tsx
@@ -2,46 +2,71 @@ import React, { useEffect, useState } from 'react';
 import { View, Text } from 'react-native';
 import NetInfo from '@react-native-community/netinfo';
 import { useTheme } from '@/theme';
+import { WifiIcon } from '@/assets/icon';
 
 const NetworkStatusBar: React.FC = () => {
-   
-    const [isInternetReachable, setInternetReachable] = useState<boolean | null>(null);
-    const [isVisible, setVisible] = useState<boolean>(false);
-    const { backgrounds, layout, gutters, colors,fonts } = useTheme();
+  const [isInternetReachable, setInternetReachable] = useState<boolean | null>(
+    null,
+  );
+  const [isVisible, setVisible] = useState<boolean>(false);
+  const [initialLoad, setInitialLoad] = useState<boolean>(true);
+  const { layout, gutters, colors, fonts } = useTheme();
 
-    useEffect(() => {
-        const unsubscribe = NetInfo.addEventListener((state) => {
-            console.log('Network state changed:', state);
-            if (state.isConnected) {
-                setInternetReachable(state.isConnected);
-                setVisible(true);
-                setTimeout(() => {
-                    setVisible(false);
-                }, 5000);
-                
+  useEffect(() => {
+    const unsubscribe = NetInfo.addEventListener((state) => {
+      const isInitialState = isInternetReachable === null;
+      console.log('Network state changed:', isInitialState, state.isConnected);
+      if (state.isConnected) {
+        if (!initialLoad && !isInternetReachable) {
+          // Show success message only after reconnection following network loss
+          setInternetReachable(true);
+          setVisible(true);
+          setTimeout(() => setVisible(false), 5000);
+        } else {
+          setInternetReachable(true);
+        }
+      } else {
+        setInternetReachable(false);
+        setVisible(true);
+      }
+      setInitialLoad(false);
+      console.log(state.isInternetReachable ? 'Online' : 'Network Error');
+    });
 
-            }
-            else {
-                setInternetReachable(false);
-                setVisible(true)
-                }
-            console.log(state.isInternetReachable ? "Online" : "Network Error");
-        });
+    return () => {
+      unsubscribe();
+    };
+  }, [isInternetReachable, initialLoad]);
 
-        return () => {
-            unsubscribe();
-        };
-    }, []);
-    if (!isVisible && isInternetReachable ) 
-        return;
-    
-    return (
-        <View style={[gutters.padding_10,layout.itemsCenter,layout.justifyCenter,layout.absolute,layout.bottom0 ,{ backgroundColor: isInternetReachable ? 'green' : 'red' ,width:"100%"}]}>
-            <Text style={[fonts.bold,fonts.gray50]}>
-                { isInternetReachable ? 'Online' : 'Network Error'}
-            </Text>
-        </View>
-    );
+    if (!isVisible && isInternetReachable) return;
+
+  return (
+    <View
+      style={[
+        layout.row,
+        layout.itemsCenter,
+        layout.justifyBetween,
+        layout.absolute,
+        layout.bottom0,
+        gutters.paddingHorizontal_16,
+        {
+          backgroundColor: isInternetReachable ? 'green' : 'red',
+          width: '100%',
+          height: 23,
+        },
+      ]}
+    >
+      <View style={[layout.flex_1]}>
+        <WifiIcon width={15} height={15} color={colors.gray00} />
+      </View>
+      <View style={[layout.justifyCenter, layout.itemsCenter, { flex: 3 }]}>
+        <Text style={[fonts.bold, fonts.gray50]}>
+          {isInternetReachable ? 'Online' : 'Network Error'}
+        </Text>
+      </View>
+      <View style={[layout.flex_1]} />
+    </View>
+  );
 };
 
 export default NetworkStatusBar;
