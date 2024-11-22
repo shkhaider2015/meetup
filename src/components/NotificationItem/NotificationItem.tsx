35859a450b187ae1c23200d96024b09d8f5838c5
unknown <alina.ahmad9898@gmail.com>
2024-11-22 18:50:24 +0500

    notification-listing

--- src/components/NotificationItem/NotificationItem.tsx
+++ src/components/NotificationItem/NotificationItem.tsx
@@ -1,24 +1,74 @@
-import { DummyLaraBeu } from "@/assets/dummyImages";
-import { useTheme } from "@/theme";
-import { fontFamily } from "@/theme/_config";
-import { INotificationItem } from "@/types/notificationItem";
-import { Image, Text, TouchableOpacity, View } from "react-native";
+import { DummyLaraBeu } from '@/assets/dummyImages';
+import { useTheme } from '@/theme';
+import { fontFamily } from '@/theme/_config';
+import { INotificationItem } from '@/types/notificationItem';
+import { Image, Linking, Text, TouchableOpacity, View } from 'react-native';
+import { convertImageURLforngRok } from '@/utils';
+import { Image as ImageComp } from '../template';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import isToday from 'dayjs/plugin/isToday'; 
+import isYesterday from 'dayjs/plugin/isYesterday';
 
 const NotificationItem = (props: INotificationItem) => {
-  const { image, user_name, created_at, notification } = props;
+  const { sender, title, message, createdAt,redirectPath } = props;
   const { fonts, gutters, layout } = useTheme();
+  dayjs.extend(relativeTime);
+  dayjs.extend(isToday);
+  dayjs.extend(isYesterday);
+
+  const formatDate = (createdAt:string) => {
+    const date = dayjs(createdAt);
+
+    if (date.isToday()) {
+      return date.format('HH:mm');
+    }
+
+    if (date.isYesterday()) {
+      return 'Yesterday';
+    }
+
+    if (date.year() === dayjs().year()) {
+      return date.format('HH:mm DD MMM'); 
+    }
+
+    return date.format('HH:mm DD MMM YYYY'); 
+  };
+  const onPressItem = () => {
+    Linking.openURL(redirectPath)
+    
+  }
 
   return (
-    <TouchableOpacity activeOpacity={.5} style={[ layout.row, layout.itemsCenter, layout.justifyStart, {columnGap: 20} ]} >
-      <View style={[ layout.flex_1 ]} >
-        <Image source={image} style={{ width: 60, height: 60 }} />
+    <TouchableOpacity
+      activeOpacity={0.5}
+      onPress={onPressItem}
+      style={[
+        layout.row,
+        layout.itemsCenter,
+        layout.justifyStart,
+        { columnGap: 20 },
+      ]}
+    >
+      <View style={[layout.flex_1]}>
+        <ImageComp
+          imageURL={convertImageURLforngRok(sender.profileImage)}
+          containerStyle={{ width: 50, height: 50, borderRadius: 40 }}
+          fastImageProp={{ style: { borderRadius: 40 } }}
+        />
       </View>
-      <View style={[ layout.col, { flex: 6, height: 55} ]} >
-        <View style={[ layout.row, layout.justifyBetween, layout.itemsCenter ]} >
-          <Text style={[ fonts.size_16, fonts.gray800, fontFamily._500_Medium ]} >{user_name}</Text>
-          <Text style={[ fonts.size_14, fontFamily._300_Light, fonts.gray250 ]} >{created_at}</Text>
+      <View style={[layout.col, { flex: 6, height: 55 }]}>
+        <View style={[layout.row, layout.justifyBetween, layout.itemsCenter]}>
+          <Text style={[fonts.size_14, fonts.gray800, fontFamily._500_Medium]}>
+            {sender?.name}
+          </Text>
+          <Text style={[fonts.size_10, fontFamily._300_Light, fonts.gray250]}>
+            {formatDate(createdAt)}
+          </Text>
         </View>
-        <Text style={[ fonts.size_12, fontFamily._400_Regular, fonts.gray250 ]} >{notification}</Text>
+        <Text style={[fonts.size_12, fontFamily._400_Regular, fonts.gray250]}>
+          {message}
+        </Text>
       </View>
     </TouchableOpacity>
   );
--- src/screens/Notification/Notification.tsx
+++ src/screens/Notification/Notification.tsx
@@ -1,15 +1,44 @@
-import { Dummy_Joe, Dummy_Johnson2, Dummy_LaraBeu2, Dummy_Maxwell, Dummy_Meetup, DummyFarnese, DummyLaraBeu, DummyMeetupBell } from "@/assets/dummyImages";
-import { NotificationItem } from "@/components";
-import { SafeScreen } from "@/components/template";
-import { useTheme } from "@/theme";
-import { heights } from "@/theme/_config";
-import { RootStackParamList } from "@/types/navigation";
-import { INotificationItem } from "@/types/notificationItem";
-import { Dimensions, FlatList, ScrollView, Text, View } from "react-native";
-import { NativeStackScreenProps } from "react-native-screens/lib/typescript/native-stack/types";
+import { useSelector } from 'react-redux'; // Add the import
+import { NotificationItem } from '@/components';
+import { SafeScreen } from '@/components/template';
+import { getNotifications } from '@/services/notifications';
+import { useTheme } from '@/theme';
+import { heights } from '@/theme/_config';
+import { RootStackParamList } from '@/types/navigation';
+import { INotificationItem } from '@/types/notificationItem';
+import { useMutation } from '@tanstack/react-query';
+import { useEffect, useState } from 'react';
+import { Dimensions, FlatList, ScrollView, Text, View } from 'react-native';
+import { NativeStackScreenProps } from 'react-native-screens/lib/typescript/native-stack/types';
+import { RootState } from '@/store';
 
 const Notifications = ({}: NotificationsScreenType) => {
+  const userId = useSelector((state: RootState) => state.user._id);
+
+  const [notificationData, setNotificationData] = useState<INotificationItem[]>(
+    [],
+  );
   const { layout, gutters, backgrounds, fonts } = useTheme();
+  const { isPending, isError, mutate } = useMutation({
+    mutationFn: () => {
+      return getNotifications(userId);
+    },
+    onSuccess: (data: INotificationItem[]) => {
+      console.log('Notifications fetched successfully:', data);
+      setNotificationData(data);
+    },
+
+    onError: (error) => {
+      console.error('Error fetching notifications:', error);
+    },
+  });
+
+  useEffect(() => {
+    console.log('userId:', userId);
+    if (userId) {
+      mutate();
+    }
+  }, [userId]);
 
   return (
     <SafeScreen>
@@ -21,99 +50,105 @@ const Notifications = ({}: NotificationsScreenType) => {
           },
         ]}
       >
-        <FlatList 
-          data={dummyData}
-          renderItem={({item}) => <NotificationItem {...item} />}
-          keyExtractor={(item, ind) => item.id || ind.toString() }
-          ItemSeparatorComponent={() => <View style={[ gutters.marginVertical_12, {height: 1, backgroundColor: '#a5a4a846'} ]}  />}
+        <FlatList
+          data={notificationData}
+          renderItem={({ item }) => <NotificationItem {...item} />}
+          keyExtractor={(item) => item._id}
+          ItemSeparatorComponent={() => (
+            <View
+              style={[
+                gutters.marginVertical_12,
+                { height: 1, backgroundColor: '#a5a4a846' },
+              ]}
+            />
+          )}
           contentContainerStyle={{
             paddingVertical: 34,
-            paddingHorizontal: 24
+            paddingHorizontal: 24,
           }}
           showsVerticalScrollIndicator={false}
         />
-        {/* <NotificationItem /> */}
       </View>
     </SafeScreen>
   );
 };
 
-const dummyData:INotificationItem[] = [
-  {
-    id: "111",
-    user_name: "Farnese Vandimion",
-    created_at: "14:28",
-    notification: "Farnese liked your photo.",
-    image: DummyFarnese
-  },
-  {
-    id: "112",
-    user_name: "Joe",
-    created_at: "1:12",
-    notification: "Joe visited your profile.",
-    image: Dummy_Joe
-  },
-  {
-    id: "113",
-    user_name: "Meetup",
-    created_at: "Yesterday",
-    notification: "Our services will be down around 4pm may 13th 2024",
-    image: Dummy_Meetup
-  },
-  {
-    id: "114",
-    user_name: "Farnese Vandimion",
-    created_at: "Yesterday",
-    notification: "Farnese liked your photo.",
-    image: DummyLaraBeu
-  },
-  {
-    id: "115",
-    user_name: "Maxwell",
-    created_at: "14:28 14 april",
-    notification: "Maxwell reacted to your activity.",
-    image: Dummy_Maxwell
-  },
-  {
-    id: "116",
-    user_name: "Meetup",
-    created_at: "14:28 12 april",
-    notification: "Alert! related your activity Skateboard park....",
-    image: DummyMeetupBell
-  },
-  {
-    id: "117",
-    user_name: "Farnese Vandimion",
-    created_at: "14:28 11 april",
-    notification: "Farnese liked your photo.",
-    image: DummyFarnese
-  },
-  {
-    id: "118",
-    user_name: "Farnese Vandimion",
-    created_at: "14:28 11 april",
-    notification: "Farnese liked your photo.",
-    image: Dummy_LaraBeu2
-  },
-  {
-    id: "119",
-    user_name: "Johnson",
-    created_at: "14:28 10 april",
-    notification: "Johnson commented on your activity.",
-    image: Dummy_Johnson2
-  },
-  {
-    id: "120",
-    user_name: "Mercy",
-    created_at: "14:28 6 april",
-    notification: "Mercy saved your activity.",
-    image: DummyFarnese
-  }
-]
+// const dummyData:INotificationItem[] = [
+//   {
+//     id: "111",
+//     user_name: "Farnese Vandimion",
+//     created_at: "14:28",
+//     notification: "Farnese liked your photo.",
+//     image: DummyFarnese
+//   },
+//   {
+//     id: "112",
+//     user_name: "Joe",
+//     created_at: "1:12",
+//     notification: "Joe visited your profile.",
+//     image: Dummy_Joe
+//   },
+//   {
+//     id: "113",
+//     user_name: "Meetup",
+//     created_at: "Yesterday",
+//     notification: "Our services will be down around 4pm may 13th 2024",
+//     image: Dummy_Meetup
+//   },
+//   {
+//     id: "114",
+//     user_name: "Farnese Vandimion",
+//     created_at: "Yesterday",
+//     notification: "Farnese liked your photo.",
+//     image: DummyLaraBeu
+//   },
+//   {
+//     id: "115",
+//     user_name: "Maxwell",
+//     created_at: "14:28 14 april",
+//     notification: "Maxwell reacted to your activity.",
+//     image: Dummy_Maxwell
+//   },
+//   {
+//     id: "116",
+//     user_name: "Meetup",
+//     created_at: "14:28 12 april",
+//     notification: "Alert! related your activity Skateboard park....",
+//     image: DummyMeetupBell
+//   },
+//   {
+//     id: "117",
+//     user_name: "Farnese Vandimion",
+//     created_at: "14:28 11 april",
+//     notification: "Farnese liked your photo.",
+//     image: DummyFarnese
+//   },
+//   {
+//     id: "118",
+//     user_name: "Farnese Vandimion",
+//     created_at: "14:28 11 april",
+//     notification: "Farnese liked your photo.",
+//     image: Dummy_LaraBeu2
+//   },
+//   {
+//     id: "119",
+//     user_name: "Johnson",
+//     created_at: "14:28 10 april",
+//     notification: "Johnson commented on your activity.",
+//     image: Dummy_Johnson2
+//   },
+//   {
+//     id: "120",
+//     user_name: "Mercy",
+//     created_at: "14:28 6 april",
+//     notification: "Mercy saved your activity.",
+//     image: DummyFarnese
+//   }
+// ]
 
 type NotificationsScreenType = NativeStackScreenProps<
   RootStackParamList,
-  "Notifications"
+  'Notifications'
 >;
 
 export default Notifications;
